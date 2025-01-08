1cd7815d94098cb5bc6a07629f7efb8fd0ac67ff
tn835286 <tn835286@gmail.com>
2025-01-08 21:40:39 +0800

    Update 認識資料類型

--- Assets/Scrlpts/Class_2_DataType.cs
+++ Assets/Scrlpts/Class_2_DataType.cs
@@ -19,4 +19,15 @@ public class Class_2_DataType : MonoBehaviour
     public string character = "蓋倫";
     public bool isDead = false;         // 是否死亡 : 否
     public bool gameOver = true;        // 是否結束 : 是
+
+    public byte lv = 16;              // byte 型別，範圍 0~255，大小 8 byte
+    public uint coin = 9999;            // uint 型別，範圍 0 ~ 4***，大小 32 byte
+    public long items = 3000;           // long 型別，範圍 0 ~ 92***，大小 64 byte
+
+    // 字串與字元
+    public string playerName = "KID";   // 字串 : 儲存多個字元，使用雙引號
+    public char a = 'a';                // 字元 : 儲存一個字元，使用單引號
+
+    // 溢位
+    // public sbyte number = 128;        // 溢位導致錯誤，會出現紅色蚯蚓
 }
