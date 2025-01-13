848a81128b5f1bcc07d78ed6e1ff9200fe506bcb
tn835286 <tn835286@gmail.com>
2025-01-13 19:18:59 +0800

    Update 完成作業練習

---
+++ Assets/practises/practise_2_DataType.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Practise_2_DataType : MonoBehaviour
+{
+    public float variableFloat = 7.7f;
+    public double variableDouble = 0.1235;
+    public decimal varibaleDecimal = 1.23456789m;
+}
