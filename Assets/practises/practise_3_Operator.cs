689647289557bc0fee9b0d8a0ab5daf6f3c72e3d
tn835286 <tn835286@gmail.com>
2025-01-20 19:01:55 +0800

    Update 作業3&4

---
+++ Assets/practises/practise_3_Operator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class practise_3_Operator : MonoBehaviour
+{
+    private void Awake()
+    {
+        // 6 / 2 (1 + 2)
+        //題目問題
+        //全部寫符號
+        //或全部省略
+        // 1 或 9
+        int answer = 6 / 2 * (1 + 2);
+        Debug.Log($"<color=#f33>6 / 2 * (1 + 2) = {answer}</color>");
+    }
+}
---
+++ Assets/practises/practise_4_Function.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 練習 : 函式
+/// </summary>
+public class practise_4_Function : MonoBehaviour 
+{
+    private void Awake()
+    {
+        Debug.Log($"<color=#3f3>布林值 True : {ReturnTrue()}</color>");
+        Debug.Log($"<color=#3f3>布林值 False : {ReturnFalse()}</color>");
+    }
+
+    /// <summary>
+    /// 布林值 True
+    /// </summary>
+    /// <returns>True</returns>
+    private bool ReturnTrue()
+    { 
+        return true;
+    }
+
+    /// <summary>
+    /// 布林值 False
+    /// </summary>
+    /// <returns>False</returns>
+    private bool ReturnFalse()
+    {
+        return false;
+    }
+}
