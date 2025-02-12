2af27cd7f4fcb0cfe9b24aef3301a01d7b020ccb
tn835286 <tn835286@gmail.com>
2025-02-12 21:03:43 +0800

    Update 靜態類別與預設建構子

--- Assets/Scrlpts/Class_7_2_Static.cs
+++ Assets/Scrlpts/Class_7_2_Static.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Kai
@@ -8,9 +9,10 @@ namespace Kai
     /// </summary>
     public class Class_7_2_Static: MonoBehaviour
     {
+        #region 變數與屬性
         //成員:
         //變數、屬性、方法
-        
+
         //非靜態變數
         public int inventoryWater = 10;
         //靜態變數:修飾詞後面添加關鍵字 Static
@@ -21,6 +23,10 @@ namespace Kai
         public string skillMain => "火球術";
         //靜態屬性
         public static string skillSecond => "治癒術";
+        #endregion
+
+        private float attack = 10;
+        private static float mp = 100;
 
         private void Awake()
         {
@@ -33,11 +39,18 @@ namespace Kai
         public void Punch()
         {
             Debug.Log("<color=#3f3>使用拳擊</color>");
+            //非竟方法內可以存取所有成員
+            Debug.Log($"<color=#f9e>非靜態攻擊力 : {attack}</color>");
+            Debug.Log($"<color=#f9e>靜態魔力: {mp}</color>");
         }
 
         public static void Kick()
         {
             Debug.Log("<color=#3f3>使用踢擊</color>");
+            //靜態方法內只能存取靜態成員
+            //由於attack是非靜態所以無法存取(導致錯誤)
+            //Debug.Log($"<color=#f9e>非靜態攻擊力 : {attack}</color>");
+            Debug.Log($"<color=#f9e>靜態魔力: {mp}</color>");  
         }
 
         private void Start()
