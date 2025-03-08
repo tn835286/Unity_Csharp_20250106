212d4bed5c92a0a2e0c2a66ada9d5b077cefab74
tn835286 <tn835286@gmail.com>
2025-03-08 16:29:44 +0800

    Updata 作業9&10

---
+++ Assets/practices/practice_9_DataType.cs
@@ -0,0 +1,18 @@
+﻿using KAI.Tools;
+using UnityEngine;
+
+namespace KAI
+{
+    /// <summary>
+    /// 資料類型轉換
+    /// </summary>
+    public class practice_9_DataType : MonoBehaviour
+    {
+        private void Awake()
+        {
+            float number = -999.321f;
+            sbyte byteNumber = (sbyte)number;
+            LogSysytem.LogWithColor(byteNumber, "#f77");
+        }
+    } 
+}
