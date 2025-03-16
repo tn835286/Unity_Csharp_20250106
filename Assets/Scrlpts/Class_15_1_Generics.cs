5b9249277994e1051f6117339498830f242f87ae
tn835286 <tn835286@gmail.com>
2025-03-16 22:59:40 +0800

    Update 堆疊及佇列

--- Assets/Scrlpts/Class_15_1_Generics.cs
+++ Assets/Scrlpts/Class_15_1_Generics.cs
@@ -11,6 +11,7 @@ namespace KAI.Class_15
     {
         private void Awake()
         {
+            #region 不使用泛型的對調方式
             int numberA = 7, numberB = 9;
             LogSysytem.LogWithColor($"數字 A 與 B :{numberA} | {numberB}", "#f93");
             SwapNumber(ref numberA, ref numberB);
@@ -25,6 +26,25 @@ namespace KAI.Class_15
             LogSysytem.LogWithColor($"物件 A 與 B :{objA} | {objB}", "#3f3");
             SwapObject(ref objA, ref objB);
             LogSysytem.LogWithColor($"物件 A 與 B :{objA} | {objB}", "#3f3");
+            #endregion
+
+            bool boolA = true, boolB = false;
+            LogSysytem.LogWithColor($"A 與 B :{boolA} | {boolB}", "#9f9");
+            Swap<bool>(ref boolA, ref boolB);
+            LogSysytem.LogWithColor($"A 與 B :{boolA} | {boolB}", "#9f9");
+
+            byte byteA = 1, byteB = 9;
+            LogSysytem.LogWithColor($"A 與 B :{byteA} | {byteB}", "#9f9");
+            Swap<byte>(ref byteA, ref byteB);
+            LogSysytem.LogWithColor($"A 與 B :{byteA} | {byteB}", "#9f9");
+
+            var player1 = new DataPlayer<int>();
+            player1.data = 99;
+            player1.LogData(123);
+
+            var player2 = new DataPlayer<string>();
+            player2.data = "玩家二號";
+            player2.LogData("哈囉");
         }
 
         /// <summary>
@@ -32,7 +52,7 @@ namespace KAI.Class_15
         /// </summary>
         /// <param name="a">第一個數字</param>
         /// <param name="b">第二個數字</param>
-        public void SwapNumber(ref int a, ref  int b)
+        public void SwapNumber(ref int a, ref int b)
         {
             int temp = a;           //將第一個數字放去旁邊(暫存)
             a = b;                  //將第二個數字放到第一個數字內
@@ -62,5 +82,98 @@ namespace KAI.Class_15
             a = b;
             b = temp;
         }
-    }   
+
+        /// <summary>
+        /// 使用泛型進行對調
+        /// </summary>
+        /// <typeparam name="T">要對調的資料類型</typeparam>
+        /// <param name="a">第一個資料</param>
+        /// <param name="b">第二個資料</param>
+        public void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        private void Start()
+        {
+            var player = new Player();
+            var enemy = new Enemy();
+            var attackEvent = new AttackEvent<Player, Enemy>();
+            attackEvent.Attack(player, enemy);
+
+            var hp = new Hp();
+            var attack = new Attack();
+            hp.Increase(10.5f);
+            attack.Increase(50);
+            hp.Increase(3.75f);
+
+            var checker = new CheckValue<Hp, float>();
+            checker.Check(hp);
+        }
+    }
+
+    public class DataPlayer<T>
+    {
+        public T data;
+
+        public void LogData(T data)
+        {
+            LogSysytem.LogWithColor(data, "#3ff");
+        }
+    }
+
+    public class Player { }
+    public class Enemy { }
+
+    public class AttackEvent<T, U>
+    {
+        public void Attack(T attacker, U defender)
+        {
+            LogSysytem.LogWithColor($"{attacker}攻擊{defender}", "#f3f");
+        }
+    }
+
+    // 泛型介面
+    public interface IStat<T>
+    {
+        public T value { get; set; }               //該狀態的值
+        public void Increase(T amount);     //增加該狀態
+    }
+
+    public class Hp : IStat<float>
+    {
+        public float value { get; set; }
+
+        public void Increase(float amount)
+        {
+            value += amount;
+            LogSysytem.LogWithColor($"血量:{value}", "#f3f");
+        }
+    }
+
+    public class Attack : IStat<int>
+    {
+        public int value { get; set; }
+
+        public void Increase(int amount)
+        {
+            value += amount;
+            LogSysytem.LogWithColor($"攻擊力:{value}", "#f3f");
+        }
+    }
+
+    //泛型約束 T 必須實作 IStat<U> 介面
+    // U 可以是任何類型
+    public class CheckValue<T, U> where T : IStat<U>
+    {
+        public void Check(T stat)
+        {
+            // 添加約束後可以使用IStat<T>成員
+            LogSysytem.LogWithColor($"狀態的值:{stat.value}", "#3d3");
+        }
+    }
+
 }
+
---
+++ Assets/Scrlpts/Class_16_1_DataStruct.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;   //有許多資料結構的命名空間
+using KAI.Tools;
+using NUnit.Framework;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace KAI.Class_16
+{
+    /// <summary>
+    /// 資料結構
+    /// </summary>
+    public class Class_16_1_DataStruct : MonoBehaviour
+    {
+        // 陣列
+        public int[] attacks = { 10, 15, 7 };
+        public float[] defens = { 1.5f, 7.5f, 0.3f };
+
+        // 清單:泛型集合
+        public List<int> speeds = new List<int>() { 3, 9, 7 };
+        public List<string> props = new List<string>() { "藥水", "地圖" };
+        public List<float> playerDefens;
+
+        private void Awake()
+        {
+            #region 清單
+            // 清單存取:與陣列相同
+            LogSysytem.LogWithColor($"第三筆速度:{speeds[2]}", "#3f3");
+            speeds[0] = 10;
+            LogSysytem.LogWithColor($"第一筆速度:{speeds[0]}", "#3f3");
+            // 陣列初始化後就無法增減長度
+            // 添加一筆道具
+            props.Add("寶劍");
+            // 刪除第一筆道具
+            props.RemoveAt(0);
+            // 添加頭盔到編號1上
+            props.Insert(1, "頭盔");
+
+            foreach (var prop in props)
+            {
+                LogSysytem.LogWithColor($"道具:{prop}", "#f93");
+            }
+
+            // 使用建構子帶入陣列
+            playerDefens = new List<float>(defens);
+            // 排序:由小到大
+            playerDefens.Sort();
+            // 反排序:由大到小
+            playerDefens.Reverse();
+
+            foreach (var item in playerDefens)
+            {
+                LogSysytem.LogWithColor($"資料:{item}", "#f93");
+            }
+
+            // Count 指清單內的資料，根據 Add 或 Remove 改變
+            LogSysytem.LogWithColor($"數量:{props.Count}", "#77f");
+            // Capcity 指清單內的容量，系統自動分配，預射為 4 並且以倍數成長(C# 各版本不同
+            LogSysytem.LogWithColor($"容量:{props.Capacity}", "#77f");
+
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                numbers.Add(i);
+                LogSysytem.LogWithColor($"數量:{numbers.Count}", "#3a3");
+                LogSysytem.LogWithColor($"容量:{numbers.Capacity}", "#f39");
+            }
+            #endregion
+
+            #region 堆疊
+            // 堆疊 : 先進後出
+            Stack<string> enemys = new Stack<string>();
+            // 放資料進入堆疊
+            enemys.Push("史萊姆");
+            enemys.Push("哥布林");
+            LogStack<string>(enemys);
+            // 拿資料並且不移除
+            var peek = enemys.Peek();
+            LogSysytem.LogWithColor(peek, "#78f");
+            LogStack<string>(enemys);
+            // 拿資料並且移除
+            var pop = enemys.Pop();
+            LogSysytem.LogWithColor(pop, "#78f");
+            LogStack<string>(enemys);
+            //判斷是否包含某筆資料
+            LogSysytem.LogWithColor($"{enemys.Contains("哥布林")}", "#3f6");
+            //清除所有資料
+            enemys.Clear();
+            LogStack<string>(enemys);
+            #endregion
+
+            // Queue佇列 : 先進先出，先放進來的資料先被拿出，回來遊戲的攻擊順序
+            Queue<string>player = new Queue<string>();
+            player.Enqueue("盜賊");
+            player.Enqueue("法師");
+            player.Enqueue("戰士");
+            LogQueue<string>(player);
+            // 拿東西不刪除，與堆疊的 Peek 相同
+            LogSysytem.LogWithColor(player.Peek(), "#f33");
+            LogQueue<string>(player);
+            // 拿東西並刪除，與堆疊的 Pop 相同
+            LogSysytem.LogWithColor(player.Dequeue(), "#f33");
+            LogQueue<string>(player);
+        }
+
+        private void LogStack<T>(Stack<T> stack)
+        {
+            LogSysytem.LogWithColor("----------------", "#fff");
+            foreach (var item in stack)
+            {
+                LogSysytem.LogWithColor($"堆疊資料:{item}", "#f77");
+            }
+        }
+
+        private void LogQueue<T>(Queue<T> queue)
+        {
+            LogSysytem.LogWithColor("------------", "#fff");
+            foreach(var item in queue)
+            {
+                LogSysytem.LogWithColor($"佇列資料:{item}", "#7f7");
+            }
+        }
+    }
+    
+}
