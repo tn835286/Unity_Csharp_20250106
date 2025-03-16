a48772ba5e077bdd4037204a0b5745f5b07a68a2
tn835286 <tn835286@gmail.com>
2025-03-16 19:44:08 +0800

    Update 添加不使用泛型的對調方法

---
+++ Assets/Scrlpts/Class_15_1_Generics.cs
@@ -0,0 +1,66 @@
+﻿using KAI.Tools;
+using UnityEditor;
+using UnityEngine;
+
+namespace KAI.Class_15
+{
+    /// <summary>
+    /// 泛型 Generics
+    /// </summary>
+    public class Class_15_1_Generics : MonoBehaviour
+    {
+        private void Awake()
+        {
+            int numberA = 7, numberB = 9;
+            LogSysytem.LogWithColor($"數字 A 與 B :{numberA} | {numberB}", "#f93");
+            SwapNumber(ref numberA, ref numberB);
+            LogSysytem.LogWithColor($"數字 A 與 B :{numberA} | {numberB}", "#f93");
+
+            char charA = '嗨', charB = '嘿';
+            LogSysytem.LogWithColor($"字元 A 與 B :{charA} | {charB}", "#f93");
+            SwapChar(ref charA, ref charB);
+            LogSysytem.LogWithColor($"字元 A 與 B :{charA} | {charB}", "#f93");
+
+            object objA = 3.5f, objB = 7.7f;
+            LogSysytem.LogWithColor($"物件 A 與 B :{objA} | {objB}", "#3f3");
+            SwapObject(ref objA, ref objB);
+            LogSysytem.LogWithColor($"物件 A 與 B :{objA} | {objB}", "#3f3");
+        }
+
+        /// <summary>
+        /// 數字對調
+        /// </summary>
+        /// <param name="a">第一個數字</param>
+        /// <param name="b">第二個數字</param>
+        public void SwapNumber(ref int a, ref  int b)
+        {
+            int temp = a;           //將第一個數字放去旁邊(暫存)
+            a = b;                  //將第二個數字放到第一個數字內
+            b = temp;               //將旁邊的數字放到第二個數字
+        }
+
+        /// <summary>
+        /// 字元對調
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public void SwapChar(ref char a, ref char b)
+        {
+            char temp = a;
+            a = b;
+            b = temp;
+        }
+
+        /// <summary>
+        /// 物件對調
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public void SwapObject(ref object a, ref object b)
+        {
+            object temp = a;
+            a = b;
+            b = temp;
+        }
+    }   
+}
---
+++ Assets/Scrlpts/Class_15_1_interfaceObject.cs
@@ -0,0 +1,67 @@
+﻿using KAI.Tools;
+using UnityEngine;
+
+namespace KAI.Class_15
+{
+    /// <summary>
+    /// 介面物件：實作介面的物件
+    /// </summary>
+    public class Class_15_1_interfaceObject
+    {
+
+    }
+
+    // 介面的使用步驟
+    // 1. 定義介面與介面成員
+    // 2. 類別實作介面
+    // 3. 實作介面的成員
+
+    // 帶有「使用」功能的介面，遊戲內可被使用的物品
+    public interface IUse
+    {
+        // 不用宣告方法主體，不用寫大括號
+        public void Use();
+    }
+
+    public interface IDestroy
+    { 
+        public void Destroy(); 
+    }
+    
+    // 步驟 2.
+    // C# 單一繼承，多重實作介面
+    public class Weapon : IUse
+    {
+        // 步驟 3.
+        public void Use()
+        {
+            LogSysytem.LogWithColor("使用武器，施展武器技能", "#fa3");
+        }
+    }
+
+    public class Potion : IUse, IDestroy
+    {
+
+        public void Destroy()
+        {
+            LogSysytem.LogWithColor("藥水使用完畢，刪除", "#f11");
+        }
+
+        public void Use()
+        {
+            LogSysytem.LogWithColor("使用藥水，恢復魔力", "#a3f");
+        }
+    }
+
+    public class Chest : IUse
+    {
+        public void Destroy()
+        {
+            LogSysytem.LogWithColor("寶箱使用完畢，刪除", "#f11");
+        }
+        public void Use()
+        {
+            LogSysytem.LogWithColor("使用寶箱，獲得隨機道具", "#af3");
+        }
+    }
+}
